67cf24e55464ee20804b5d6d66fca6ca8b6c8c2f
Ernesto Alejandro Santana Pérez <ernestoa7723@icloud.com>
2022-08-21 00:58:53 -0400

    cli version move snake eat apple

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 const Snake = require('./snake')
+const readline = require('readline');
 
 const Matrix = {
     toString: xsxs => xsxs.map(xs => xs.join(' ')).join('\r\n'),
@@ -6,7 +7,7 @@ const Matrix = {
 
     set: val => pos => adjust(pos.y)(adjust(pos.x)(k(val))),
     addSnake: state => pipe(...map(Matrix.set('X'))(state.snake)),
-    addApple: state => Matrix.set('o')(state.apple),
+    addApple: state => Matrix.set('O')(state.apple),
 
     fromState: state => pipe(
         Matrix.make,
@@ -19,6 +20,21 @@ let State = Snake.initialState()
 
 const show = () => console.log('\x1Bc' + Matrix.toString(Matrix.fromState(State)))
 
+const step = () => State = Snake.next(State)
+
+readline.emitKeypressEvents(process.stdin);
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') process.exit()
+    switch (key.name.toUpperCase()) {
+        case 'A': case 'LEFT': State = Snake.enqueue(State, Snake.west); break
+        case 'W': case 'UP': State = Snake.enqueue(State, Snake.north); break
+        case 'D': case 'RIGHT': State = Snake.enqueue(State, Snake.east); break
+        case 'S': case 'DOWN': State = Snake.enqueue(State, Snake.south); break
+    }
+});
+
 setInterval(() => {
     show()
+    step();
 }, 100)
